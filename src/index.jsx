4375cde2c92404331da1a1c39fba2bb3e4424395
Yunita Dwi Liandarwati <ydliandarwati@gmail.com>
2024-07-03 02:15:17 +0200

    fix for deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom';
 import App from "./App";
 import "./style/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>
+	</HashRouter>
 );
